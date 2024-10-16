1a9da459d63d4b0a6c9fa0453833226a26b91818
rramalhoo <raphael.athenesi@gmail.com>
2024-10-16 08:32:03 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function ex1() {
+    const a=[];
+    somat=0;
+
+    for(i=0;i<=9;i++){
+        a[i]=parseFloat(prompt(`Digite o ${i+1}º elemento do array`));
+        somat+=a[i];
+    }
+    alert(`Soma dos elementos ${somat}`);
+}
+
+function ex2() {
+    const a=[];
+    const b=[];
+    for(i=0;i<=9;i++){
+        a[i]=parseFloat(prompt(`Digite o ${i+1}º elemento do array`));
+        if (a[i]%2==0){
+            b.push(a[i])
+        }
+    }
+    alert(b)
+}
